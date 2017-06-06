81fdadb0706476d5942797790272deb741d6bc79
stef <stefdev49@gmail.com>
2017-06-06 08:16:24 +0200

    MAX_FEED_RATE update
    - match value from last firmware (6/2017)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -538,7 +538,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 4, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 4, 170 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
